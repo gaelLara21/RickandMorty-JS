ba10957a22c1eeb653ad96a95177a5822a7ab8e3
gaelLara21 <gaellara19@gmail.com>
2022-10-24 11:11:49 -0500

    Inplementando cards en boostrap

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const urlBase = 'https://rickandmortyapi.com/api/character/';
 
+const LoadData  = (urlBase) =>{
 fetch (urlBase)
   .then(respuesta => {
       return respuesta.json();
@@ -11,12 +12,32 @@ fetch (urlBase)
       console.log(personajes);
       showCharacters(personajes);
   })
+}
 
   const showCharacters = (personajes) =>{
-      const ListaPersonajes = document.querySelector('ul');
+      const ListaPersonajes = document.querySelector('#characters');
       personajes.forEach(personaje => {
-          const li = document.createElement('li');
-          li.innerText = `personaje ${personaje.name}`;
-          ListaPersonajes.appendChild(li);
+          const div = document.createElement('div');
+          div.classList.add('col');
+          div.classList.add('m-2');
+          div.innerHTML = creaCard(personaje);
+          ListaPersonajes.appendChild(div);
       })
   }
+
+  const creaCard = (personaje) => {
+      const html = `
+  <div class="card" style="width: 18rem;">
+  <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
+  <div class="card-body">
+    <h5 class="card-title"> Is${personaje.name}</h5>
+    <p class="card-text"> Is${personaje.status}</p>
+    <a href="#" class="btn btn-primary">ver mas</a>
+  </div>
+</div>
+     
+      `;
+      return html;
+  }
+
+  LoadData(urlBase);
